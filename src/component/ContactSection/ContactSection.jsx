d19afc61ee29960ffa04525053d0cace1d51d433
YahYa. <108167288+iAmYaxYa@users.noreply.github.com>
2024-12-01 11:34:41 +0300

    Update contact map

--- src/component/ContactSection/ContactSection.jsx
+++ src/component/ContactSection/ContactSection.jsx
@@ -8,7 +8,7 @@ const ContactSection = () => {
       <div className="flex justify-between items-center flex-wrap gap-5">
         <div className="w-full lg:w-[48%]">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.30067538451!2d45.31032025981659!3d2.0355470854350175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3d584355ad12f43f%3A0xaf42842fe0ed44be!2sSOMTAC!5e0!3m2!1sen!2sso!4v1678525685464!5m2!1sen!2sso"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51660121.10301331!2d163.77319335937494!3d37.78808138412046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085807e8df70a15%3A0x20d9be370efb666e!2sUdemy!5e0!3m2!1sen!2sug!4v1733041815613!5m2!1sen!2sug"
             className="w-full h-96 lg:h-[320px] dark:invert border border-slate-400"
           ></iframe>
           <div className="mt-5">
@@ -18,8 +18,7 @@ const ContactSection = () => {
                   <HiLocationMarker className="text-2xl" />
                 </div>
                 <h1 className="text-xl text-slate-900 dark:text-white">
-                  Taleh, North Building of Masjid Abdirashid , <br /> Mogadishu,
-                  somalia, 252
+                  600 Harrison St, San Francisco, CA 94107,  <br /> United States
                 </h1>
               </div>
               <div className="flex gap-3 items-center">
@@ -27,7 +26,7 @@ const ContactSection = () => {
                   <FaPhone className="text-xl" />
                 </div>
                 <h1 className="text-xl text-slate-900 dark:text-white">
-                  61 0754219 <br />
+                  5892935253 <br />
                   <span className="text-sm text-slate-700 dark:text-slate-300">
                     Mobile
                   </span>
