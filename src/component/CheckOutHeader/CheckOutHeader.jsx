1ba3b5de8826040c56b50c7eff9a7105abbb9bf5
YahYa. <108167288+iAmYaxYa@users.noreply.github.com>
2024-12-01 15:57:20 +0300

    Update CheckOutHeader

--- src/component/CheckOutHeader/CheckOutHeader.jsx
+++ src/component/CheckOutHeader/CheckOutHeader.jsx
@@ -10,12 +10,12 @@ const CheckOutHeader = ({ title }) => {
                  font-bold text-4xl"
         to="/"
       >
-        Som
+        Ud
         <span
           className="text-slate-900
                   dark:text-sky-500"
         >
-          Tac.
+          emy.
         </span>
       </Link>
       <div>
